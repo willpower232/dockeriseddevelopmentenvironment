5649cd8d360f0740654fb098e32ec934027ff1bf
William Hall <github@willpoweredinc.co.uk>
2019-01-21 15:20:50 +0000

    well thats awkward

--- nginx/gogo.sh
+++ nginx/gogo.sh
@@ -3,8 +3,8 @@
 # https://stackoverflow.com/a/4774063
 SCRIPT_DIR="$(cd "$(dirname "$0")"; pwd -P)"
 
-source $SCRIPT_DIR/../gogo_network.sh
- 
+source $SCRIPT_DIR/../networks/php.sh
+
 docker run -dit --restart always \
 	--name nginx \
 	-p 80:80 -p 443:443\
