e85718139b49b99f751a66bf49f15e4841316919
William Hall <github@willpoweredinc.co.uk>
2019-01-23 17:39:34 +0000

    use dnsmasq in PHP

--- phpfpm/gogo.sh
+++ phpfpm/gogo.sh
@@ -9,10 +9,21 @@ if [[ "$(docker images -q phpfpm71:latest 2> /dev/null)" == "" ]]; then
 	docker build -t phpfpm71 $SCRIPT_DIR
 fi
 
+# use DNSMASQ so it can resolve the .zz addresses to the host for assets in PDFs
+# whilst the fallback seems to work, it might not be the best solution
+# so if not using DNSMASQ then remove the --dns line below
+
+# try and get the dnsmasq ip address or the default if it fails
+DNSMASQ="$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' dnsmasq 2>/dev/null || echo '127.0.0.11')"
+
+# trim whitespace at the start
+DNSMASQ="$(echo $DNSMASQ)"
+
 docker run -dit --restart always \
 	--name phpfpm \
 	-h phpfpm \
-	--network=$NETWORKNAME \
+	--network $NETWORKNAME \
+	--dns $DNSMASQ \
 	-v ${HOME}/.aws/credentials:${HOME}/.aws/credentials:ro \
 	-v ${HOME}/gitrepos:${HOME}/gitrepos \
 	-v $SCRIPT_DIR/config/bashrc:${HOME}/.bashrc:ro \
