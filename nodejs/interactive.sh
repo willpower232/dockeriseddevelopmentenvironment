9f5e2e1d5cc5351cc2cb6ae63fbefbdce4b2a9c7
William Hall <github@willpoweredinc.co.uk>
2019-01-30 10:53:31 +0000

    allow the nodejs interactive script to access a running container in case user got themselves messed up

--- nodejs/interactive.sh
+++ nodejs/interactive.sh
@@ -1,17 +1,27 @@
 #!/bin/bash
 
+CONTAINERNAME="nodejs10"
+
 # https://stackoverflow.com/a/4774063
 SCRIPT_DIR="$(cd "$(dirname "$0")"; pwd -P)"
 
-if [[ "$(docker images -q nodejs10:latest 2> /dev/null)" == "" ]]; then
-        docker build -t nodejs10 $SCRIPT_DIR
+if [[ "$(docker images -q $CONTAINERNAME:latest 2> /dev/null)" == "" ]]; then
+	docker build -t $CONTAINERNAME $SCRIPT_DIR
 fi
 
-docker run -it --rm \
-	--name nodejs \
-	-h nodejs \
-        -v $SCRIPT_DIR/config/bashrc:${HOME}/.bashrc:ro \
-        -v ${HOME}/gitrepos:${HOME}/gitrepos \
-	-w "$PWD" \
-       nodejs10 \
-       bash
+# as the container is created with --rm, it will be removed
+# when the service stops or the first process exits
+# but if there is one running already, we can just hop into it
+# rather than try to recreate an existing container
+if [ ! "$(docker ps | grep $CONTAINERNAME)" ]; then
+	docker run -it --rm \
+		--name $CONTAINERNAME \
+		-h $CONTAINERNAME \
+		-v $SCRIPT_DIR/config/bashrc:${HOME}/.bashrc:ro \
+		-v ${HOME}/gitrepos:${HOME}/gitrepos \
+		-w "$PWD" \
+		$CONTAINERNAME \
+		bash
+else
+	docker exec -it -w "$PWD" $CONTAINERNAME bash
+fi
