38c7a6be3501bdaae7653e082f04ea05b1885a3b
William Hall <github@willpoweredinc.co.uk>
2019-03-12 17:17:58 +0000

    ruby ruby ruby ruby

---
+++ ruby/README.md
@@ -0,0 +1,15 @@
+# Ruby
+
+## Interactive
+
+Provides a Ruby-enabled terminal to run sass and AWS SDK-based Ruby scripts.
+
+I have an alias in my `.bashrc` along the lines of
+
+```bash
+alias bashnode='bash ~/containers/ruby/interactive.sh'
+```
+
+### Root Access
+
+Add `-u 0` to the command in `interactive.sh`.
---
+++ ruby/interactive.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+CONTAINERNAME="ruby23"
+
+# https://stackoverflow.com/a/4774063
+SCRIPT_DIR="$(cd "$(dirname "$0")"; pwd -P)"
+
+if [[ "$(docker images -q $CONTAINERNAME:latest 2> /dev/null)" == "" ]]; then
+	docker build -t $CONTAINERNAME $SCRIPT_DIR
+fi
+
+# as the container is created with --rm, it will be removed
+# when the service stops or the first process exits
+# but if there is one running already, we can just hop into it
+# rather than try to recreate an existing container
+if [ ! "$(docker ps | grep $CONTAINERNAME)" ]; then
+	docker run -it --rm \
+		--name $CONTAINERNAME \
+		-h $CONTAINERNAME \
+		-v ${HOME}/.aws:${HOME}/.aws:ro \
+		-v $SCRIPT_DIR/config/bashrc:${HOME}/.bashrc:ro \
+		-v ${HOME}/gitrepos:${HOME}/gitrepos \
+		-w "$PWD" \
+		$CONTAINERNAME \
+		bash
+else
+	docker exec -it -w "$PWD" $CONTAINERNAME bash
+fi
