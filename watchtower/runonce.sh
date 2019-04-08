28ba0024d515fb7fecefbbd6b2c2772d72c5d517
William Hall <github@willpoweredinc.co.uk>
2019-04-08 10:16:35 +0100

    use correct watchtower image and timeout watchtower process

--- watchtower/runonce.sh
+++ watchtower/runonce.sh
@@ -6,9 +6,13 @@ if [ ! "$(docker ps | grep $CONTAINERNAME)" ]; then
 	# https://stackoverflow.com/a/4774063
 	SCRIPT_DIR="$(cd "$(dirname "$0")"; pwd -P)"
 
+	docker pull containrrr/watchtower
+
+	( sleep 30 ; docker stop $CONTAINERNAME ) &
+
 	docker run --rm -it \
 		--name $CONTAINERNAME \
 		-v /var/run/docker.sock:/var/run/docker.sock \
-		v2tec/watchtower \
+		containrrr/watchtower \
 		--interval 10 --cleanup
 fi
