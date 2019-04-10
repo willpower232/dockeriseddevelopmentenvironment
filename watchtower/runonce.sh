19efedd1834baad29a13f74c5e2fa98e0a5baea9
William Hall <github@willpoweredinc.co.uk>
2019-04-10 09:02:34 +0100

    fix timezone and remove unused thing

--- watchtower/runonce.sh
+++ watchtower/runonce.sh
@@ -3,9 +3,6 @@
 CONTAINERNAME="watchtower"
 
 if [ ! "$(docker ps | grep $CONTAINERNAME)" ]; then
-	# https://stackoverflow.com/a/4774063
-	SCRIPT_DIR="$(cd "$(dirname "$0")"; pwd -P)"
-
 	docker pull containrrr/watchtower
 
 	( sleep 30 ; docker stop $CONTAINERNAME ) &
@@ -13,6 +10,7 @@ if [ ! "$(docker ps | grep $CONTAINERNAME)" ]; then
 	docker run --rm -it \
 		--name $CONTAINERNAME \
 		-v /var/run/docker.sock:/var/run/docker.sock \
+		-e TZ=Europe/London \
 		containrrr/watchtower \
 		--interval 10 --cleanup
 fi
