9c07a401e965c0fa7ee1dd74108d3dbb2a178505
William Hall <github@willpoweredinc.co.uk>
2024-02-18 15:02:43 +0000

    what if mariadb

--- mysql/gogo.sh
+++ mysql/gogo.sh
@@ -6,11 +6,11 @@ SCRIPT_DIR="$(cd "$(dirname "$0")"; pwd -P)"
 source $SCRIPT_DIR/../networks/php.sh
 
 docker run -dit --restart always \
-	--name mysql \
-	--network-alias mysql-docker \
+	--name mariadb \
+	--network-alias mariadb-docker \
 	-p 3306:3306 \
 	--network $NETWORKNAME \
 	-v $SCRIPT_DIR/data:/var/lib/mysql \
 	-v $SCRIPT_DIR/config:/etc/mysql/conf.d \
 	-e MYSQL_ALLOW_EMPTY_PASSWORD=true \
-	mysql:5.7
+	mariadb:10.11
